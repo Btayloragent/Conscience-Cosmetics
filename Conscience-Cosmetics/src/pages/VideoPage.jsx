1c4e8ab8c2f2635f73e3f76ffa1245fe63744e40
Brandon <btayloragent@gmail.com>
2024-09-02 19:13:24 -0400

    sidebar

--- Conscience-Cosmetics/src/pages/VideoPage.jsx
+++ Conscience-Cosmetics/src/pages/VideoPage.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import VideoCard from '../components/VideoCard';
 import './VideoPage.css'; 
 import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
 
 const VideoPage = () => {
     const containerStyle = {
-        marginTop: '205px', // Adjust this value to set the desired space
+        marginTop: '40px', // Adjust this value to set the desired space
+        display: 'flex', // Use flexbox for layout
     };
 
     return (
         <>
             <NavBar />
             <div className="video-page-container" style={containerStyle}>
-                <div className="video-grid">
+                <SideBar style={{ width: '80px' }} /> {/* Set a fixed width for the sidebar */}
+                <div className="video-grid" style={{ flex: 1, paddingLeft: '20px' }}> {/* Adjust padding for space between sidebar and grid */}
                     <div className="video-card-container">
                         <VideoCard />
                     </div>
